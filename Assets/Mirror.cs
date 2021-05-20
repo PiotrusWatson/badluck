31a93c099a90fb828689c9a8c6adcc68da40700a
Piotrus Watson <piotruswatson@gmail.com>
2021-05-20 22:05:18 +0100

    added bad mirror

---
+++ Assets/Mirror.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mirror : MonoBehaviour
+{
+
+    public Transform mirrorCam;
+    public Transform playerCam;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Camera playerCamera = playerCam.GetComponentInChildren<Camera>();
+        if (playerCamera != null){
+            playerCam = playerCamera.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = (playerCam.position - transform.position).normalized;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+
+        rotation.eulerAngles = transform.eulerAngles - rotation.eulerAngles;
+
+        mirrorCam.localRotation = rotation;
+    }
+
+}
